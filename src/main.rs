f671eba2223b0848ecde5ca7d5b8a364d9d69eec
Samu <el.tobal@gmail.com>
2023-12-29 09:36:33 +0100

    update main test

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 use clap::Parser;
-use tokio::select;
 use tokio::sync::Mutex;
 use tokio_util::sync::CancellationToken;
+use tracing::{error, info, Level};
+use tracing_subscriber::FmtSubscriber;
 
 use std::net::SocketAddr;
 use std::sync::Arc;
@@ -15,14 +16,14 @@ use tokio_util::bytes::BytesMut;
 #[command(author, version, about, long_about = None)]
 struct Args {
     /// Local host for clients to connect and get data pushed
-    #[arg(short = 'h', long, default_value_t = String::from("localhost"), env = "HOST")]
+    #[arg(short = 'i', long, default_value_t = String::from("localhost"), env = "HOST")]
     local_host: String,
     /// Local port for clients to connect and get data pushed
     #[arg(short = 'p', long, default_value_t = 8080u16, env = "PORT")]
     local_port: u16,
 
     /// Remote host to pull data from
-    #[arg(short = 'r', long, env = "REMOTE_HOST")]
+    #[arg(short = 'j', long, env = "REMOTE_HOST")]
     remote_host: String,
 
     /// Remote port to pull data from
@@ -32,33 +33,11 @@ struct Args {
 
 const BUFFER_SIZE: usize = 1024;
 
-async fn run(
-    local_host: String,
-    local_port: u16,
-    remote_host: String,
-    remote_port: u16,
-    cancel: CancellationToken,
-) -> Result<()> {
-    let source = format!("{remote_host}:{remote_port}")
-        .parse::<SocketAddr>()
-        .expect("Failed to parse remote host address");
-
-    let source = TcpStream::connect(source)
-        .await
-        .expect("Failed to connect to remote host");
-
+async fn run(stream: TcpStream, listener: TcpListener, cancel: CancellationToken) -> Result<()> {
     let sinks: Arc<Mutex<Vec<TcpStream>>> = Arc::new(Mutex::new(vec![]));
 
-    let listener = format!("{local_host}:{local_port}")
-        .parse::<SocketAddr>()
-        .expect("Failed to parse local host address");
-
-    let listener = TcpListener::bind(listener)
-        .await
-        .expect("Failed to bind TCP listener");
-
     let mut buffer = BytesMut::with_capacity(BUFFER_SIZE);
-    let mut reader = BufReader::new(source);
+    let mut reader = BufReader::new(stream);
 
     loop {
         let read = reader.read_buf(&mut buffer);
@@ -72,28 +51,35 @@ async fn run(
                 let mut sinks = sinks.lock().await;
                 let mut failed_sink_indexes = vec![];
 
+                let read = buffer.split_to(n);
+
                 for (index, sink) in  sinks.iter_mut().enumerate() {
 
-                    let  mut data = buffer.split_to(n).freeze();
+                    let addr = sink.peer_addr().unwrap();
+
+                    let mut data = read.clone().freeze();
+
                     let mut writer = BufWriter::new(sink);
 
                     let mut m = 0;
 
-                     while m < n {
+                    while m < n {
+
                         m += match writer.write_buf(&mut data).await {
                             Err(e) => {
-                                eprintln!("Error writing: {}", e);
+                                error!("Error writing to {}: {}", addr,  e);
                                 failed_sink_indexes.push(index);
                                 break;
                             },
                             Ok(0) => {
+                                info!("Could not write to {}, disconnected", addr);
                                 // Ok(0) means nothing was be written, so most likely the client socket disconnected and needs to be removed
                                 failed_sink_indexes.push(index);
                                 break;
                             },
-
-                            Ok(m) => {
-                                m
+                            Ok(r) => {
+                                println!("read something {}", &r);
+                                r
                             },
                         }
                     }
@@ -116,6 +102,12 @@ async fn run(
 
 #[tokio::main]
 async fn main() -> Result<()> {
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::TRACE)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
     let Args {
         local_port,
         local_host,
@@ -125,15 +117,25 @@ async fn main() -> Result<()> {
 
     println!("running with passed args {:?}", Args::parse());
 
+    let stream = format!("{remote_host}:{remote_port}")
+        .parse::<SocketAddr>()
+        .expect("Failed to parse remote host address");
+
+    let stream = TcpStream::connect(stream)
+        .await
+        .expect("Failed to connect to remote host");
+
+    let listener = format!("{local_host}:{local_port}")
+        .parse::<SocketAddr>()
+        .expect("Failed to parse local host address");
+
+    let listener = TcpListener::bind(listener)
+        .await
+        .expect("Failed to bind TCP listener");
+
     let cancel = CancellationToken::new();
 
-    tokio::spawn(run(
-        local_host,
-        local_port,
-        remote_host,
-        remote_port,
-        cancel.clone(),
-    ));
+    tokio::spawn(run(stream, listener, cancel.clone()));
 
     tokio::signal::ctrl_c().await?;
 
@@ -144,6 +146,9 @@ async fn main() -> Result<()> {
 
 #[cfg(test)]
 mod tests {
+
+    use std::{thread::sleep, time::Duration};
+
     use super::*;
     use tokio::net::TcpStream;
 
@@ -151,59 +156,72 @@ mod tests {
     async fn run_test() {
         // create a tests server
 
-        let local_host = "127.0.0.1".to_string();
-        let local_port = 8082;
-
-        let remote_host = "127.0.0.1".to_string();
-        let remote_port = 9092;
-
-        let remote_addr = format!("{}:{}", remote_host, remote_port);
-
-        let remote = TcpListener::bind(remote_addr.clone()).await.unwrap();
-
-        let mut sink_1 = TcpStream::connect(remote_addr.clone()).await.unwrap();
-        let mut sink_2 = TcpStream::connect(remote_addr.clone()).await.unwrap();
-
         let cancel = CancellationToken::new();
 
-        tokio::spawn(run(
-            local_host,
-            local_port,
-            remote_host,
-            remote_port,
-            cancel.clone(),
-        ));
+        let listener_addr = "127.0.0.1:8081";
+        let stream_addr = "127.0.0.1:9091";
 
-        let test_data = vec![1, 2, 3];
+        let stream_listener = TcpListener::bind(&stream_addr).await.unwrap();
+        let listener = TcpListener::bind(&listener_addr).await.unwrap();
+
+        let data = vec![1, 2, 3];
 
         tokio::spawn({
-            let data = test_data.clone();
+            let test_data = data.clone();
+
             async move {
-                // Read data from the stream
+                let mut remote_stream = stream_listener.accept().await.unwrap().0;
+
+                println!("after connect");
+
+                let r = remote_stream.write(&test_data).await.unwrap();
+
+                println!("after sending");
+
+                assert_eq!(r, test_data.len());
+            }
+        });
+
+        sleep(Duration::from_secs(2));
+
+        let stream = TcpStream::connect(&stream_addr).await.unwrap();
+
+        tokio::spawn(run(stream, listener, cancel.clone()));
+
+        sleep(Duration::from_secs(2));
+
+        let handle_1 = tokio::spawn({
+            let data = data.clone();
+
+            let mut sink = TcpStream::connect(&listener_addr).await.unwrap();
+
+            async move {
                 let mut buffer = [0u8; BUFFER_SIZE];
-                let n = sink_1.read(&mut buffer).await.unwrap();
+                let n = sink.read(&mut buffer).await.unwrap();
+
+                assert_eq!(buffer[..n].to_vec(), data);
+                assert_eq!(buffer[..n].to_vec(), vec![1, 2]);
+            }
+        });
+
+        let handle_2 = tokio::spawn({
+            let data = data.clone();
+
+            let mut sink = TcpStream::connect(&listener_addr).await.unwrap();
+
+            async move {
+                let mut buffer = [0u8; BUFFER_SIZE];
+                let n = sink.read(&mut buffer).await.unwrap();
 
                 assert_eq!(buffer[..n].to_vec(), data);
             }
         });
 
-        tokio::spawn({
-            let data = test_data.clone();
+        handle_1.await.unwrap();
+        handle_2.await.unwrap();
 
-            async move {
-                // Read data from the stream
-                let mut buffer = [0u8; BUFFER_SIZE];
-                let n = sink_2.read(&mut buffer).await.unwrap();
+        // let (r, s) = join!(handle_1, handle_2);
 
-                assert_eq!(buffer[..n].to_vec(), data);
-            }
-        });
-
-        let mut remote_stream = remote.accept().await.unwrap().0;
-        let r = remote_stream.write(&test_data).await.unwrap();
-
-        cancel.cancel();
-
-        assert_eq!(r, test_data.len());
+        cancel.cancel()
     }
 }
