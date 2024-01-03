b4921cc4108815e7f27f10de9123f00671fe6598
Samu <el.tobal@gmail.com>
2024-01-03 08:13:14 +0100

    Refactor async assert counter

--- src/test.rs
+++ src/test.rs
@@ -3,20 +3,21 @@ use once_cell::sync::Lazy;
 use rand::Rng;
 use std::{
     future::Future,
-    sync::atomic::{AtomicUsize, Ordering},
+    sync::{
+        atomic::{AtomicUsize, Ordering},
+        Arc,
+    },
     thread::sleep,
     time::Duration,
 };
-use tokio::{
-    io::{AsyncReadExt, AsyncWriteExt},
-    try_join,
-};
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
-struct WaitForTest<const N: usize>(AtomicUsize);
+#[derive(Clone)]
+struct WaitForTest<const N: usize>(Arc<AtomicUsize>);
 
 impl<const N: usize> WaitForTest<N> {
     pub fn new() -> Self {
-        Self(AtomicUsize::new(0))
+        Self(Arc::new(AtomicUsize::new(0)))
     }
     fn add(&self, n: usize) {
         self.0.fetch_add(n, Ordering::Relaxed);
@@ -45,28 +46,29 @@ impl<const N: usize> Future for WaitForTest<N> {
 
 #[test_log::test(tokio::test)]
 async fn test_reader_to_tx() {
+    // prepare test data
     const DATA_SIZE: usize = 1024;
 
     let mut data = [0_u8; DATA_SIZE];
     let mut rng = rand::thread_rng();
     data.iter_mut().for_each(|b| *b = rng.gen());
 
+    // setup async assert counter
     static COUNT_ASSERTS: Lazy<WaitForTest<1>> = Lazy::new(WaitForTest::new);
 
+    // setup function under test
     let (tx, _) = tokio::sync::broadcast::channel(1);
-
     let (reader, mut writer) = tokio::io::duplex(BUFFER_SIZE);
-
-    tokio::spawn(reader_to_tx(reader, tx.clone()));
+    tokio::spawn(reader_to_tx(reader, tx.clone())); // <- function under test
 
     // give `reader_to_tx` a break to wire everything up
     sleep(Duration::from_secs(1));
 
-    let write = tokio::spawn(async move {
+    tokio::spawn(async move {
         let _ = writer.write(&data).await.unwrap();
     });
 
-    let read = tokio::spawn({
+    tokio::spawn({
         let mut rx = tx.subscribe();
 
         async move {
@@ -76,31 +78,31 @@ async fn test_reader_to_tx() {
         }
     });
 
-    try_join!(write, read).unwrap();
-
+    COUNT_ASSERTS.clone().await;
     assert!(COUNT_ASSERTS.check());
 }
 
 #[test_log::test(tokio::test)]
 async fn test_tx_to_writer() {
+    // prepare test data
     const DATA_SIZE: usize = 1024;
-
-    static COUNT_ASSERTS: Lazy<WaitForTest<1>> = Lazy::new(WaitForTest::new);
 
     let mut data = [0_u8; DATA_SIZE];
     let mut rng = rand::thread_rng();
     data.iter_mut().for_each(|b| *b = rng.gen());
 
+    // setup async assert counter
+    static COUNT_ASSERTS: Lazy<WaitForTest<1>> = Lazy::new(WaitForTest::new);
+
+    // setup function under test
     let (tx, _) = tokio::sync::broadcast::channel(1);
-
     let (mut reader, writer) = tokio::io::duplex(BUFFER_SIZE);
-
-    tokio::spawn(tx_to_writer(writer, tx.clone()));
+    tokio::spawn(tx_to_writer(writer, tx.clone())); // <- function under test
 
     // give `tx_to_writer` a break to wire everything up
     sleep(Duration::from_secs(1));
 
-    let read = tokio::spawn(async move {
+    tokio::spawn(async move {
         let mut received = vec![0; DATA_SIZE];
         reader.read_exact(&mut received).await.unwrap();
 
@@ -108,13 +110,12 @@ async fn test_tx_to_writer() {
         COUNT_ASSERTS.add(1);
     });
 
-    let send = tokio::spawn(async move {
+    tokio::spawn(async move {
         let data_bytes = Bytes::from(data[..].to_vec());
         tx.send(data_bytes).unwrap();
     });
 
-    try_join!(read, send).unwrap();
-
+    COUNT_ASSERTS.clone().await;
     assert!(COUNT_ASSERTS.check());
 }
 
@@ -128,8 +129,7 @@ async fn tcp_broadcast_test() {
     let mut rng = rand::thread_rng();
     data.iter_mut().for_each(|b| *b = rng.gen());
 
-    static COUNT_ASSERTS: Lazy<WaitForTest<{ 2 * NUM_CHUNKS }>> = Lazy::new(WaitForTest::new);
-
+    // helper function to mock a client
     async fn launch_client(listener_addr: &str, data: [u8; CHUNK_SIZE * NUM_CHUNKS]) {
         let mut sink = TcpStream::connect(listener_addr).await.unwrap();
         let mut count_read = 0;
@@ -145,6 +145,12 @@ async fn tcp_broadcast_test() {
             count_read += n;
         }
     }
+
+    const NUM_CLIENTS: usize = 2;
+
+    // setup async assert counter
+    static COUNT_ASSERTS: Lazy<WaitForTest<{ NUM_CLIENTS * NUM_CHUNKS }>> =
+        Lazy::new(WaitForTest::new);
 
     // bind addresses to mock servers and clients
     let listener_addr = "127.0.0.1:9081"; // <-- tests TCP clients will connect here
@@ -163,18 +169,19 @@ async fn tcp_broadcast_test() {
 
     let stream = TcpStream::connect(&stream_addr).await.unwrap();
 
-    tokio::spawn(tcp_broadcaster(stream, listener));
+    tokio::spawn(tcp_broadcaster(stream, listener)); // <- function under test
 
     // give `tcp_broadcaster` a break to wire things up
     sleep(Duration::from_secs(1));
-
     tokio::spawn(TcpStream::connect(listener_addr));
 
+    // launch mock clients
     let client_1 = tokio::spawn(launch_client(listener_addr, data));
     let client_2 = tokio::spawn(launch_client(listener_addr, data));
 
     try_join!(client_1, client_2).unwrap();
 
+    COUNT_ASSERTS.clone().await;
     assert!(COUNT_ASSERTS.check());
 }
 
@@ -189,8 +196,7 @@ async fn udp_broadcast_test() {
     let mut rng = rand::thread_rng();
     data.iter_mut().for_each(|b| *b = rng.gen());
 
-    static COUNT_ASSERTS: Lazy<WaitForTest<{ 2 * NUM_CHUNKS }>> = Lazy::new(WaitForTest::new);
-
+    // helper function to mock a client
     async fn launch_client(listener_addr: &str, data: [u8; CHUNK_SIZE * NUM_CHUNKS]) {
         let mut sink = TcpStream::connect(listener_addr).await.unwrap();
         let mut count_read = 0;
@@ -207,6 +213,12 @@ async fn udp_broadcast_test() {
         }
     }
 
+    const NUM_CLIENTS: usize = 2;
+
+    // setup async assert counter
+    static COUNT_ASSERTS: Lazy<WaitForTest<{ NUM_CLIENTS * NUM_CHUNKS }>> =
+        Lazy::new(WaitForTest::new);
+
     // bind addresses to mock servers and clients
     let listener_addr = "127.0.0.1:8081"; // <-- TCP server address, used to listen to incoming TCP connections
     let read_addr = "127.0.0.1:8091"; // <-- UDP socket address, used to listen to messages that will be distributed to TCP connections at previous address
@@ -219,17 +231,22 @@ async fn udp_broadcast_test() {
     write_socket.connect(read_addr).await.unwrap();
 
     tokio::spawn(udp_broadcaster(read_socket, listener));
+
+    // give `udp_broadcaster` a break to wire things up
     sleep(Duration::from_secs(1));
 
+    // launch mock clients
     let client_1 = tokio::spawn(launch_client(listener_addr, data));
     let client_2 = tokio::spawn(launch_client(listener_addr, data));
 
-    let sender = tokio::task::spawn(async move {
+    // send test data
+    let sender = tokio::spawn(async move {
         sleep(Duration::from_secs(1));
         write_socket.send(&data).await.unwrap();
     });
 
     try_join!(client_1, client_2, sender).unwrap();
 
+    COUNT_ASSERTS.clone().await;
     assert!(COUNT_ASSERTS.check());
 }
